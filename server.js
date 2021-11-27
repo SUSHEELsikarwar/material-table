50ed1415858ad350ca5c37cf612b6071003ea15e
susheel.singh-a <susheel.singh@innoeye.com>
2021-11-27 18:14:33 +0530

    fix: dist path

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-material-table'));
+app.use(express.static(__dirname + '/docs'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/angular-material-table/index.html'));
+res.sendFile(path.join(__dirname+'/docs/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
